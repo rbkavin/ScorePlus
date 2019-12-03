66bdc4e9ea4e7e7cd798d9625ad4406cd5537160
kavin kumar <bkavin001@gmail.com>
2019-12-03 21:58:55 +0530

    Update script.js

--- Scoreupdate/scripts/script.js
+++ Scoreupdate/scripts/script.js
@@ -1,9 +1,5 @@
 const s = require("Scene");
 const p = require("Patches");
 
-var score = p.getScalarValue("score");
 
-p.getScalarValue("score").monitor().subscribe(()=>{
-    s.root.find(p.getStringValue("textname").pinLastValue()).text = score.toString();
-
-});
+s.root.find(p.getStringValue("textname").pinLastValue()).text = p.getScalarValue("score").toString();
